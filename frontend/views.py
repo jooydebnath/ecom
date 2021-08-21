0c47cbc341158c8a389fc1e7ef51daacee75eb0d
Joy Debnath <joy91740@gmail.com>
2021-08-22 00:02:34 +0600

    create models, register admin, add vieiws

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import *
 
-# Create your views here.
+
+def products(request):
+    productss = Product.objects.all()
+    return render(request, 'products.html', {'productss': productss})
